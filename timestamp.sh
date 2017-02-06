221d0e2d8336fab4181b8b68933fa2d4c35a1086
St33v on arc <st33v@f3rr3t.com>
2017-02-06 20:20:56 +1100

    working version of timestamp script.

---
+++ timestamp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+# Extract timestamp from exif data and write it on the photo.
+# Called from another script (camfreshen.sh)
+# SJP 6 Feb 2017
+
+# Grab the timestamp
+timestamp=" $(identify -format '%[exif:datetime]' ${1})"
+ echo timestamp = [${timestamp}]
+
+# Add the text to the image
+# stolen from:
+# http://www.imagemagick.org/Usage/annotating/
+convert -font DejaVu-Sans-Mono -pointsize 36 -size 600x100 xc:none -gravity west\
+	-stroke black -strokewidth 2 -annotate 0 "${timestamp}" \
+	-background none -shadow 110x3+0+0 +repage \
+	-stroke none -fill yellow -annotate 0 "${timestamp}" \
+	${1} +swap -gravity southwest -geometry +0-3 \
+	-composite  out.jpg
